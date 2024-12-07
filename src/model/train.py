50021ff25829c7be3c1b6dc3bdba2178068f8cea
SinglaSandeep <98082681+SinglaSandeep@users.noreply.github.com>
2024-12-07 17:47:03 +0530

    Update train.py

--- src/model/train.py
+++ src/model/train.py
@@ -56,12 +56,6 @@ def split_data(df):
     Returns:
         X_train, X_test, y_train, y_test: Split features and target sets.
     """
-    if "target" not in df.columns:
-        raise RuntimeError("Dataframe does not contain a 'target' column.")
-
-    X = df.drop("target", axis=1)
-    y = df["target"]
-
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
     return X_train, X_test, y_train, y_test
